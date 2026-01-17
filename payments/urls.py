0344d0661dd6059399ec110ea1b0a0002e80e003
denisa <denisa.zoulova30@gmail.com>
2026-01-17 17:14:07 +0100

    fix_webhook

--- payments/urls.py
+++ payments/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = "payments"
 
 urlpatterns = [
+    path("stripe-webhook/", views.stripe_webhook, name="stripe-webhook"),
     path("checkout/<slug:plan_code>/", views.checkout, name="checkout"),
     path("success/", views.success, name="success"),
     path("cancel/", views.cancel, name="cancel"),
